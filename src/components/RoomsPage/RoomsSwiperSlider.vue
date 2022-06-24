4b8af409717d447b02cf4359a771dbcf57f67194
Sergio00001 <digtyarsergio@gmail.com>
2022-06-24 16:14:59 +0300

    add Swiper.js

--- src/components/RoomsPage/RoomsSwiperSlider.vue
+++ src/components/RoomsPage/RoomsSwiperSlider.vue
@@ -135,10 +135,8 @@ export default {
   },
   setup() {
     const onSwiper = (swiper) => {
-      //   console.log(swiper)
     }
     const onSlideChange = () => {
-      //   console.log('slide change')
     }
     return {
       onSwiper,
